d326e4a2ea5643515d8051d54570a434c62fddf5
martin.bonetti <1martinbonetti@gmail.com>
2024-02-07 10:29:28 +0100

    more testing

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from ossapi import Ossapi
+import ossapi
+import tkinter as tk
 
 secret = input("enter token : ")
 
-api = Ossapi(30107, secret)
+api = ossapi.Ossapi(30107, secret)
 
-print(api.user(19587231, mode="osu").username)
-print(api.beatmap(221777).beatmapset_id)
-scores = api.user_scores(19587231, "best")
-for i in scores:
-    print(i.pp)
+# print(api.user(19587231, mode="osu").username)
+# print(api.beatmap(221777).beatmapset_id)
+# scores = api.user_scores(19587231, "best")
+# for i in scores:
+#     print(i.pp)
+
+
+window = tk.Tk()
+username = tk.Label(text = f"username : {api.user(19587231, mode="osu").username}")
+username.pack()
+
+topplays = api.user_scores(19587231, "best")
+
+tk.Label(text = f"top play : {topplays[0].beatmapset.title} {topplays[0].pp}pp").pack()
+window.mainloop()
